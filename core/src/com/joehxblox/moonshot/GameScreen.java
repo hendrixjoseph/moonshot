9ec7347ce3eb56db0e8a5cf7ab0c7e9e53547adb
hendrixjoseph <hendixjoseph@aol.com>
2020-11-23 12:46:19 -0500

    scale tile map to screen; code cleanup

--- core/src/com/joehxblox/moonshot/GameScreen.java
+++ core/src/com/joehxblox/moonshot/GameScreen.java
@@ -10,7 +10,6 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.maps.MapProperties;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
@@ -20,15 +19,11 @@ import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.Predicate;
 
-import java.util.Iterator;
+import static com.badlogic.gdx.maps.tiled.TiledMapTileLayer.Cell;
 
 public class GameScreen extends ScreenAdapter {
-    private static final float WIDTH = 800f;
-    private static final float HEIGHT = 480f;
-
     private final Moonshot game;
 
-    private final TiledMap tiledMap;
     private final OrthographicCamera camera = new OrthographicCamera();
     private final OrthogonalTiledMapRenderer tiledMapRenderer;
     private final SpriteBatch sb;
@@ -38,8 +33,9 @@ public class GameScreen extends ScreenAdapter {
     private final Rectangle centerButton;
     private final Rectangle rightButton;
 
-
     private final BitmapFont font = new BitmapFont();
+
+    private final float scale;
 
     public GameScreen(final Moonshot game) {
         this.game = game;
@@ -49,35 +45,40 @@ public class GameScreen extends ScreenAdapter {
 
         this.camera.setToOrtho(false, w, h);
         this.camera.update();
-        this.tiledMap = new TmxMapLoader().load("moonshot.tmx");
-        this.tiledMapRenderer = new OrthogonalTiledMapRenderer(this.tiledMap);
+        final TiledMap tiledMap = new TmxMapLoader().load("moonshot.tmx");
+
+        final int mapHeight = tiledMap.getProperties().get("height", Integer.class);
+        final int tileHeight = tiledMap.getProperties().get("tileheight", Integer.class);
+        this.scale = h / (mapHeight * tileHeight);
+
+        this.tiledMapRenderer = new OrthogonalTiledMapRenderer(tiledMap, this.scale);
 
         this.sb = new SpriteBatch();
         this.moon = new Sprite(new Texture(Gdx.files.internal("moon.png")));
         this.moon.translateY(64.01f);
         this.moon.translateX(10);
 
-        float buttonWidth = w * 0.25f;
-        leftButton = new Rectangle(0, 0, buttonWidth, h);
-        centerButton = new Rectangle(buttonWidth, 0, w - 2 * buttonWidth, h);
-        rightButton = new Rectangle(w - buttonWidth, 0, buttonWidth, h);
-    }
+        final float buttonWidth = w * 0.25f;
+        this.leftButton = new Rectangle(0, 0, buttonWidth, h);
+        this.centerButton = new Rectangle(buttonWidth, 0, w - 2 * buttonWidth, h);
+        this.rightButton = new Rectangle(w - buttonWidth, 0, buttonWidth, h);
 
-    private TiledMapTileLayer.Cell getTileCellAt(final float screenX, final float screenY) {
-        final Vector3 coords = this.camera.unproject(new Vector3(screenX, screenY, 0));
+        Gdx.input.setInputProcessor(new InputAdapter() {
+            @Override
+            public boolean touchDown(final int screenX, final int screenY, final int pointer, final int button) {
+                final Cell cell = getTileCellAt(screenX, screenY);
+                cell.setRotation((cell.getRotation() + 1) % 4);
 
-        final int x = (int) Math.floor(coords.x / 32.0);
-        final int y = (int) Math.floor(coords.y / 32.0);
-
-        final TiledMapTileLayer ml = (TiledMapTileLayer) this.tiledMapRenderer.getMap().getLayers().get(0);
-        return ml.getCell(x, y);
+                return false;
+            }
+        });
     }
 
     @Override
     public void render(final float delta) {
-        Predicate<TiledMapTileLayer.Cell> predicate = new Predicate<TiledMapTileLayer.Cell>() {
+        final Predicate<Cell> predicate = new Predicate<Cell>() {
             @Override
-            public boolean evaluate(TiledMapTileLayer.Cell arg0) {
+            public boolean evaluate(final Cell arg0) {
                 return isCellFloor(arg0);
             }
         };
@@ -92,8 +93,8 @@ public class GameScreen extends ScreenAdapter {
         final boolean cannotPanRight = this.tiledMapRenderer.getViewBounds().x + motion > getMapWidth() - this.tiledMapRenderer.getViewBounds().width;
         final boolean cannotPanLeft = this.tiledMapRenderer.getViewBounds().x - motion < 0;
 
-        if (Gdx.input.isKeyPressed(Input.Keys.LEFT) || rectanglePressed(leftButton)) {
-            final Array<TiledMapTileLayer.Cell> cell = getTilesToTheLeft(this.moon, motion);
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT) || rectanglePressed(this.leftButton)) {
+            final Array<Cell> cell = getTilesToTheLeft(this.moon, motion);
 
             if (!cell.select(predicate).iterator().hasNext()) {
                 if (cannotPanLeft || cannotPanRight && this.moon.getX() > 250) {
@@ -108,8 +109,8 @@ public class GameScreen extends ScreenAdapter {
             this.moon.rotate(motion);
         }
 
-        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT) || rectanglePressed(rightButton)) {
-            final Array<TiledMapTileLayer.Cell> cell = getTilesToTheRight(this.moon, motion);
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT) || rectanglePressed(this.rightButton)) {
+            final Array<Cell> cell = getTilesToTheRight(this.moon, motion);
 
             if (!cell.select(predicate).iterator().hasNext()) {
                 if (cannotPanRight || cannotPanLeft && this.moon.getX() < 250) {
@@ -124,21 +125,22 @@ public class GameScreen extends ScreenAdapter {
             this.moon.rotate(-motion);
         }
 
-        if (Gdx.input.isKeyPressed(Input.Keys.UP) || rectanglePressed(centerButton)) {
+        if (Gdx.input.isKeyPressed(Input.Keys.UP) || rectanglePressed(this.centerButton)) {
             if (this.moon.getY() + this.moon.getHeight() + motion < Gdx.graphics.getHeight()) {
                 this.moon.translateY(motion);
             }
         } else {
-            final Array<TiledMapTileLayer.Cell> cell = getTilesBeneath(this.moon, motion);
+            final Array<Cell> cell = getTilesBeneath(this.moon, motion);
 
             if (cell.contains(null, true)) {
-                this.moon.setPosition(10, 64);
+                this.moon.setPosition(10, 64); // lose
             } else if (!cell.select(predicate).iterator().hasNext()) {
                 this.moon.translateY(-motion);
             }
         }
 
         this.camera.update();
+
         this.tiledMapRenderer.setView(this.camera);
         this.tiledMapRenderer.render();
 
@@ -149,72 +151,85 @@ public class GameScreen extends ScreenAdapter {
         this.sb.end();
     }
 
-    private boolean rectanglePressed(Rectangle rectangle) {
+    private boolean rectanglePressed(final Rectangle rectangle) {
         return Gdx.input.isTouched() && rectangle.contains(Gdx.input.getX(), Gdx.input.getY());
     }
 
-    private Array<TiledMapTileLayer.Cell> getTilesToTheRight(final Sprite sprite, final float motion) {
+    private Cell getTileCellAt(final float screenX, final float screenY) {
+        final Vector3 coords = this.camera.unproject(new Vector3(screenX, screenY, 0));
 
-        Array<TiledMapTileLayer.Cell> cells = new Array<>();
+        final int tilewidth = this.tiledMapRenderer.getMap()
+                                .getProperties().get("tilewidth", Integer.class);
+        final int tileHeight = this.tiledMapRenderer.getMap()
+                                .getProperties().get("tileheight", Integer.class);
+
+        final int x = (int) Math.floor(coords.x / (tilewidth * this.scale));
+        final int y = (int) Math.floor(coords.y / (tileHeight * this.scale));
+
+        final TiledMapTileLayer ml = (TiledMapTileLayer) this.tiledMapRenderer.getMap()
+                                                                        .getLayers().get(0);
+        return ml.getCell(x, y);
+    }
+
+    private Array<Cell> getTilesToTheRight(final Sprite sprite, final float motion) {
+
+        final Array<Cell> cells = new Array<>();
 
         final float x = sprite.getX() + sprite.getWidth() + motion;
         final float y = Gdx.graphics.getHeight() - sprite.getY();
 
-        cells.add(getTileCellAt(x, y),
-                getTileCellAt(x, y - sprite.getHeight() / 2),
-                getTileCellAt(x, y - sprite.getHeight()));
+        cells.add(getTileCellAt(x, y), getTileCellAt(x, y - sprite.getHeight() / 2), getTileCellAt(x, y - sprite.getHeight()));
 
         return cells;
     }
 
-    private Array<TiledMapTileLayer.Cell> getTilesToTheLeft(final Sprite sprite, final float motion) {
+    private Array<Cell> getTilesToTheLeft(final Sprite sprite, final float motion) {
 
-        Array<TiledMapTileLayer.Cell> cells = new Array<>();
+        final Array<Cell> cells = new Array<>();
 
-        float x = sprite.getX() - motion;
-        float y = Gdx.graphics.getHeight() - sprite.getY();
+        final float x = sprite.getX() - motion;
+        final float y = Gdx.graphics.getHeight() - sprite.getY();
 
-        cells.add(getTileCellAt(x, y),
-                getTileCellAt(x, y - sprite.getHeight() / 2),
-                getTileCellAt(x, y - sprite.getHeight()));
+        cells.add(getTileCellAt(x, y), getTileCellAt(x, y - sprite.getHeight() / 2), getTileCellAt(x, y - sprite.getHeight()));
 
         return cells;
     }
 
-    private Array<TiledMapTileLayer.Cell> getTilesBeneath(final Sprite sprite, final float motion) {
+    private Array<Cell> getTilesBeneath(final Sprite sprite, final float motion) {
 
-        Array<TiledMapTileLayer.Cell> cells = new Array<>();
+        final Array<Cell> cells = new Array<>();
 
-        float x = sprite.getX();
-        float y = Gdx.graphics.getHeight() - sprite.getY() + motion;
+        final float x = sprite.getX();
+        final float y = Gdx.graphics.getHeight() - sprite.getY() + motion;
 
-        cells.add(getTileCellAt(x, y),
-                  getTileCellAt(x + sprite.getWidth() / 2, y),
-                  getTileCellAt(x + sprite.getWidth(), y));
+        cells.add(getTileCellAt(x, y), getTileCellAt(x + sprite.getWidth() / 2, y), getTileCellAt(x + sprite.getWidth(), y));
 
         return cells;
     }
 
-    private boolean isCellFloor(final TiledMapTileLayer.Cell cell) {
+    private boolean isCellFloor(final Cell cell) {
         return cell != null && cell.getTile().getProperties().get("floor", false, Boolean.class);
     }
 
     private void printDebug() {
         final String spriteInfo = String.format("Sprite: %f, %f", this.moon.getX(), this.moon.getY());
-        final String mapInfo = String.format("Map: total width: %d; w,h: %f, %f; x,y: %f, %f", getMapWidth(), this.tiledMapRenderer.getViewBounds().width, this.tiledMapRenderer.getViewBounds().height, this.tiledMapRenderer.getViewBounds().x, this.tiledMapRenderer.getViewBounds().y);
-        this.font.draw(this.sb, String.format("%s%n%s", spriteInfo, mapInfo), 10, 470);
+        final String mapInfo = String.format("Map: total width: %f; w,h: %f, %f; x,y: %f, %f", getMapWidth(), this.tiledMapRenderer.getViewBounds().width, this.tiledMapRenderer.getViewBounds().height, this.tiledMapRenderer.getViewBounds().x, this.tiledMapRenderer.getViewBounds().y);
+        final String mouseInfo = String.format("Mouse: %d, %d", Gdx.input.getX(), Gdx.input.getY());
+
+
+        this.font.draw(this.sb, String.format("%s%n%s%n%s", spriteInfo, mapInfo, mouseInfo), 10, 470);
     }
 
-    private int getMapWidth() {
-        final int width = (int) this.tiledMap.getProperties().get("width");
-        final int tilewidth = (int) this.tiledMap.getProperties().get("tilewidth");
+    private float getMapWidth() {
+        final int width = this.tiledMapRenderer.getMap().getProperties().get("width", Integer.class);
+        final int tilewidth = this.tiledMapRenderer.getMap().getProperties().get("tilewidth", Integer.class);
 
-        return width * tilewidth;
+        return width * tilewidth * this.scale;
     }
 
     @Override
     public void dispose() {
         this.sb.dispose();
-        this.tiledMap.dispose();
+        this.tiledMapRenderer.dispose();
     }
 }
--- core/src/com/joehxblox/moonshot/Moonshot.java
+++ core/src/com/joehxblox/moonshot/Moonshot.java
@@ -1,15 +1,10 @@
 package com.joehxblox.moonshot;
 
 import com.badlogic.gdx.Game;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Moonshot extends Game {
     @Override
     public void create() {
-
         this.setScreen(new GameScreen(this));
     }
-
-
 }
