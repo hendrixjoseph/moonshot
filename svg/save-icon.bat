6c68464b8d0dff1cc43484d4f2f69a47ca6c7b0e
hendrixjoseph <hendixjoseph@aol.com>
2020-11-29 21:49:07 -0500

    add script to generate icons

---
+++ svg/save-icon.bat
@@ -0,0 +1,8 @@
+$env:Path += ";C:\Program Files\Inkscape\bin"
+
+inkscape --export-filename=../android/res/drawable-hdpi/ic_launcher.png -C --export-width=72 icon.svg
+inkscape --export-filename=../android/res/drawable-mdpi/ic_launcher.png -C --export-width=48 icon.svg
+inkscape --export-filename=../android/res/drawable-xhdpi/ic_launcher.png -C --export-width=96 icon.svg
+inkscape --export-filename=../android/res/drawable-xxhdpi/ic_launcher.png -C --export-width=144 icon.svg
+inkscape --export-filename=../android/res/drawable-xxxhdpi/ic_launcher.png -C --export-width=192 icon.svg
+inkscape --export-filename=../android/ic_launcher-web.png -C --export-width=512 icon.svg
